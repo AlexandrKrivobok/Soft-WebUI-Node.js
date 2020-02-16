492e39c79952e52c01c3163c0ea6a0a4c3ea2dac
Krivobok Alexandr <krivobok.olexandr@gmail.com>
2020-02-16 19:50:08 +0200

    fixed HWfor les02

--- les02/HW/task.js
+++ les02/HW/task.js
@@ -17,21 +17,25 @@ console.log(myConcat('Hello', ',', 'World!'));
 // ---------------------- lastIndexOf() ----------------------
 
 function myLastIndexOf(sourse, subStr, index = sourse.length - 1) {
+  let isMatch = false;
   for (let i = index; i >= 0; i--) {
     if (sourse[i] === subStr[subStr.length - 1]) {
-      let subStrLength = subStr.length - 2;
+      let subStrIndex = subStr.length - 2;
       let sourseOrder = 0;
       
-      for (let j = subStrLength; j >= 0; j--) {
+      for (let j = subStrIndex; j >= 0; j--) {
         sourseOrder++;
         
-        if (subStr[j] !== sourse[i - sourseOrder]) { 
+        if (subStr[j] !== sourse[i - sourseOrder]) {
+          isGood = false;
           break;
         }
         
-        return i - subStr.length + 1;
+        isMatch = true;
       }
     }
+
+    if (isMatch) return i - subStr.length + 1;
   }
 
   return -1;
@@ -39,20 +43,108 @@ function myLastIndexOf(sourse, subStr, index = sourse.length - 1) {
 
 console.log(myLastIndexOf('qwerty', 'wer', '4'));
 console.log(myLastIndexOf('qwerty', 'asawer', '4'));
+console.log(myLastIndexOf('qweraswerjjdkls', 'wer'));
 
 // ---------------------- includes() ----------------------
 
-function myIncludes(sourse, subStr) {
-  let sourseMax = sourse.length - 1;
+function myIncludes(sourse, subStr, index = 0) {
+  let sourseMaxIndex = sourse.length - 1;
+  let isMatch = false;
 
-  for (let i = 0; i < sourseMax; i++) {
+  if (index >= sourse.length) return false;
+
+  for (let i = index; i < sourseMaxIndex; i++) {
     if (sourse[i] === subStr[0]) {
-      let subStrMax = subStr.length - 1;
-      for (let j = 1; j < subStr.length - 1; j++) {
+      let subStrMaxIndex = subStr.length;
+
+      for (let j = 1; j < subStrMaxIndex; j++) {
         if (subStr[j] !== sourse[i + j]) {
+          isMatch = false;
           break;
         }
+
+        isMatch = true;
       }
+
+      if (isMatch) return true;
     }
   }
+
+  return false;
 }
+
+console.log(myIncludes('hellow, world!', 'world'));
+console.log(myIncludes('hellow, world!', 'world', 3));
+console.log(myIncludes('qwerty qwerty  ll  ll  gg', 'erty ', 7));
+
+// ---------------------- repeat() ----------------------
+
+function myRepeat(sourse, count) {
+  let intCount = parseInt(count);
+
+  if (intCount < 1) throw new RangeError('Out of range, use positive numbers');
+
+  let result = '';
+
+  for (let i = 0; i < intCount; i++) {
+    result += sourse;
+  }
+
+  return result;
+}
+
+console.log(myRepeat('hi', 5));
+console.log(myRepeat('hi', 2.5));
+// console.log(myRepeat('hi', 0));
+// console.log(myRepeat('hi', -5));
+
+// ---------------------- substr() ----------------------
+
+function mySubstr(sourse, start, length = sourse.length - start) {
+  let startIndex = start;
+  let result = '';
+
+  if (start < 0) {
+    startIndex = sourse.length + start;
+    length = sourse.length - startIndex;
+  }
+
+  for (let i = 0; i <= length - 1; i++) {
+    result += sourse[startIndex + i];
+  }
+
+  return result;
+}
+
+
+console.log(mySubstr('hello!', 0, 2)); // he
+console.log(mySubstr('Hello!', 3, 1)); // l
+console.log(mySubstr('Hello!', 2));    // llo!
+console.log(mySubstr('Hello!', -2));   // o!
+
+// ---------------------- substring() ----------------------
+
+function mySubstring(sourse, start = 0, end = sourse.length) {
+  if (start < 0 || end < 0) throw new Error('Out of range, use positive numbers');
+
+  if (start > end) {
+    a = end;
+    end = start;
+    start = a;
+  }
+
+  console.log(start, end);
+
+  let result = '';
+
+  for (let i = start; i < end; i++) {
+    result += sourse[i];
+  }
+
+  return result;
+}
+
+console.log(mySubstring('hello!', 0, 2)); // he
+console.log(mySubstring('Hello!', 3, 1)); // el
+console.log(mySubstring('Hello!', 2));    // llo!
+// console.log(mySubstring('Hello!', -2));   // o!

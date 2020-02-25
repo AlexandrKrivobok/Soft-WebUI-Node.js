91847b61c73b899f074706903b1fdd71ddb180f6
Krivobok Alexandr <krivobok.olexandr@gmail.com>
2020-02-25 12:29:55 +0200

    fixed -minutes bug

--- les10/task2.js
+++ les10/task2.js
@@ -1,15 +1,18 @@
 function countdown(msecs) {
-  let sign = msecs < 0 ? '' : '+';
-  let hours   = parseInt(msecs / 3600000).toString();
+  let sign = msecs < 0 ? '-' : '+';
+
+  const absms = Math.abs(msecs);
+
+  let hours   = parseInt(absms / 3600000).toString();
   hours = hours.length > 1? hours: '0' + hours;
 
-  let minutes = parseInt(msecs % 3600000 / 60000).toString();
+  let minutes = parseInt(absms % 3600000 / 60000).toString();
   minutes = minutes.length > 1? minutes: '0' + minutes;
 
-  let seconds = parseInt(msecs % 360000 % 60000 / 1000).toString();
+  let seconds = parseInt(absms % 360000 % 60000 / 1000).toString();
   seconds = seconds.length > 1? seconds: '0' + seconds;
 
-  return `${sign}${hours}:${minutes}:${seconds}`
+  return `${sign}:${hours}:${minutes}:${seconds}`
 }
 
 console.log(countdown(-154800000))  // return  '-43:00:00'
